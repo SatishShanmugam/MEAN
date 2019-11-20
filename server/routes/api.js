9fe42e5f88454790940f6bdd28058d8e92aaea11
SatishShanmugam <36922452+SatishShanmugam@users.noreply.github.com>
2019-11-20 18:52:44 +0530

    Update api.js
    updated api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,12 @@
 //like the importing statements 
 const express = require('express')
 const mongoose = require('mongoose')
-const db = "mongodb+srv://Satish:rggZjcypRntujcgj@pkdummy-oksws.mongodb.net/test?retryWrites=true&w=majority"
+//importing player module here to use
+const Player = require('../models/player')
+//importing object id to use for get request
+const ObjectId = require('mongoose').Types.ObjectId;
+//connection string in which PKCricket is the database name
+const db = "mongodb+srv://Satish:rggZjcypRntujcgj@pkdummy-oksws.mongodb.net/PKCricket?retryWrites=true&w=majority"
 //mongoDB connection
 mongoose.connect(db,err=>{
     if(err){
@@ -13,10 +18,35 @@ mongoose.connect(db,err=>{
 //router configuration
 const router = express.Router()
 
-//dummy get call when just api is mentioned
+// get call when just api is mentioned
 router.get('/',(req,res)=>{
-     res.send('Sent From MEAN API')  
+    // res.send('Sent From MEAN API') 
+    Player.find((err,docs)=>{
+        if(!err){res.send(docs);}
+      }); 
  })
-
+// get particular id call when just api is mentioned
+ router.get('/:id',(req,res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send(`No record with id:${req.parms.id}`)
+    }
+    else{
+        Player.findById(req.params.id,(err,docs)=>{
+            if(!err){res.send(docs);}
+          }); 
+    }
+ })
+//post method to save new play
+ router.post('/addplayer',(req,res)=>{
+    let userData = req.body
+    let user = new Player(userData)
+    user.save((error,addPlayer)=>{
+        if(error){
+            console.log('Error:'+error)
+        }else{
+            res.status(200).send(addPlayer)
+        }
+    })
+})
  //exporting router
  module.exports = router
